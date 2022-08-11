affe20406fff543b5dbf9e666fefc5751c8ba16b
SaToGo <stoledano97@gmail.com>
2022-08-11 23:05:55 +0200

    Add DrawBoard Method inside GameBoard Object

--- scr.js
+++ scr.js
@@ -1,6 +1,29 @@
+
 //GameBoard will be a unique object that saves the board.
 const GameBoard = (function() {
-    let gameBoard = []; //Array that will control the board. 
+    let gameBoard = [
+        [1, 0, 1],
+        [1, 1, 0],
+        [0, 1, 0]]; //Array that will control the board. 0 => O and 1 => X.
+
+
+    // Function To Draw the board.
+    const DrawBoard = () => {
+        let cells = document.getElementsByClassName('cell');
+
+        for(let i = 0; i < cells.length; i++) {
+        
+            //We use x and y to access a matrix which index go from 0 to 2 
+            //depending on the number 'i' that goes from 0 to 8.
+            let x = Math.floor(i/3);
+            let y = i%3; 
+            
+            if(gameBoard[x][y] == 1) cells[i].innerHTML = 'X'; 
+            else cells[i].innerHTML = 'O'; 
+        }
+    }
+
+    return {DrawBoard};
 })();
 
 //GameFlow will be a unique object that will control the game.
@@ -12,3 +35,5 @@ const GameFlow = (function(){
 const PlayerFactory = () => {
 
 }
+
+GameBoard.DrawBoard();
