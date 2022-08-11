4fc33cb5641d54c7127335184a5032b2119b8891
SaToGo <stoledano97@gmail.com>
2022-08-11 21:28:49 +0200

    Made the declaration of the Objects for the game

---
+++ scr.js
@@ -0,0 +1,14 @@
+//GameBoard will be a unique object that saves the board.
+const GameBoard = (function() {
+    let gameBoard = []; //Array that will control the board. 
+})();
+
+//GameFlow will be a unique object that will control the game.
+const GameFlow = (function(){
+
+})()
+
+// PlayerFactory will define our players.
+const PlayerFactory = () => {
+
+}
